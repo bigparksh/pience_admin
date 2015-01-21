d183e38f42086995dc664f80e2b5b956b347e2b6
kyunga <kyunga@seedtime.co>
2015-01-21 16:35:43 +0900

    코드 정리

--- server/routes/noserv_tenant.js
+++ server/routes/noserv_tenant.js
@@ -5,13 +5,8 @@ var querystring = require('querystring');
 var client = require('http');
 
 exports.login = (function(req, res) {
-  var options = get_options();
-  options.method = 'GET';
-  options.path = '/1/login?' + querystring.stringify(req.body);
-
-  console.log(options.path);
   var buffer = '';
-  client.get(options, function(result) {
+  client.get(set_options('GET', '/1/login?' + querystring.stringify(req.body)), function(result) {
     result.setEncoding('utf8');
     result.on('data', function(chunk) {
       buffer += chunk.toString();
@@ -23,23 +18,22 @@ exports.login = (function(req, res) {
 });
 
 exports.signup = (function(req, res) {
-  var options = get_options();
-  options.method = 'POST';
-  options.headers["Content-Type"] = "application/json";
-  options.path = '/1/users?' + querystring.stringify(req.body);
   var self = this;
-  client.request(options, function() {
+  client.request(set_options('POST', '/1/users?' + querystring.stringify(req.body)), function() {
     self.get(res);
   }).end();
 });
 
-function get_options() {
+function set_options(method, path) {
   return  {
     hostname: 'www.noserv.com',
     port: 2337,
+    method: method,
+    path: path,
     headers: {
       "X-Noserv-Application-Id": "rsnq1adhhj90be29fj6pz6dg6zrdlsor",
-      "X-Noserv-REST-API-Key": "txyewqfpjxvwjyviz3sp1vbm3v5qm2t9"
+      "X-Noserv-REST-API-Key": "txyewqfpjxvwjyviz3sp1vbm3v5qm2t9",
+      "Content-Type": "application/json"
     }
   };
 }
